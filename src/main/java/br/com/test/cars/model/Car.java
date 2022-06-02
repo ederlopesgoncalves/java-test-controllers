2b117d0e93ebaae106057e05d80df65331d8703f
Eder Goncalves <ederlg@ciandt.com>
2022-06-02 17:35:19 -0300

    first commit

--- src/main/java/br/com/test/cars/model/Car.java
+++ src/main/java/br/com/test/cars/model/Car.java
@@ -2,11 +2,13 @@ package br.com.test.cars.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class Car {
 
     private Long code;
